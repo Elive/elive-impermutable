37ac6c43930e6365e3b98c449940d69f4dad4917
bilibop project <quidame@poivron.org>
2012-10-12 23:22:21 +0200

    lib/bilibop/common.sh: add FSF address in the licence header.

--- lib/bilibop/common.sh
+++ lib/bilibop/common.sh
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+# Or write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307  USA.
 #
 # On Debian systems, the complete text of the GNU General
 # Public License version 3 can be found in "/usr/share/common-licenses/GPL-3".
